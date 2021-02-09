4038ab71ac491fc86963f36c63e9fe587c3fd0f9
Emmanuel Lampe <mail@emmanuel-lampe.de>
2021-02-09 16:11:10 +0100

    Replaced laravel http with guzzle

--- src/Wrapper/WrapperClient.php
+++ src/Wrapper/WrapperClient.php
@@ -3,6 +3,8 @@
 namespace RexlManu\Pterodactyl\Wrapper;
 
 use Exception;
+use GuzzleHttp\Client;
+use GuzzleHttp\RequestOptions;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Str;
 use RexlManu\Pterodactyl\Helper\ConvertHelper;
@@ -12,6 +14,7 @@ class WrapperClient
 
   private $token;
   private $section;
+  private $client;
 
   /**
    * WrapperClient constructor.
@@ -26,11 +29,12 @@ class WrapperClient
     if (! Str::endsWith(config('pterodactyl.auth.host'), '/') && ! Str::endsWith($this->section, '/')) {
       $this->section = '/' . $this->section;
     }
+    $this->client = new Client();
   }
 
   private function buildUrl($endpoint)
   {
-    if (! str_starts_with($endpoint, '/')) {
+    if (! Str::start($endpoint, '/')) {
       $endpoint = '/' . $endpoint;
     }
     return config('pterodactyl.auth.host') . '/api' . $this->section . $endpoint;
@@ -38,14 +42,17 @@ class WrapperClient
 
   public function request($method, $endpoint, $data)
   {
-    $response = Http::withToken($this->token)
-      ->contentType('application/json')
-      ->{$method}($this->buildUrl($endpoint), $data);
-    $responseJson = $response->json();
+    $response = $this->client->request($method, $this->buildUrl($endpoint), [
+      RequestOptions::BODY => $data,
+      RequestOptions::HEADERS => [
+        'Content-Type' => 'application/json'
+      ]
+    ]);
+    $responseJson = $response->getBody();
     if (is_null($responseJson)) {
       $responseJson = [];
     }
-    $responseJson[ 'status' ] = $response->status();
+    $responseJson[ 'status' ] = $response->getStatusCode();
     return json_decode(json_encode(ConvertHelper::convert($responseJson)));
   }
 
